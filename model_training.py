1df79a4828c13ec24ef00d9ed79c9cdf38af79ea
ninakira <ninkirakosyan@gmail.com>
2021-03-28 20:56:39 +0400

    KerasTrain class implementation

---
+++ model_training.py
@@ -0,0 +1,110 @@
+import datetime
+import os
+import tensorflow as tf
+
+class KerasTrain:
+    def __init__(self, model=None,
+                 name=None,
+                 description=None,
+                 train_data=None,
+                 valid_data=None,
+                 batch_size=23,
+                 from_logits=True,
+                 epochs=400,
+                 current_epoch=0,
+                 lr=0.001,
+                 lr_scheduler=None,
+                 optimizer=None,
+                 loss=None,
+                 metrics=["accuracy"],
+                 save_path="../SavedModels/",
+                 with_cp_save=True,
+                 cp_freq=20,
+                 cp_dir="checkpoints/",
+                 with_tensorboard=True,
+                 tb_dir="tf_logs/",
+                 tb_hist_freq=10):
+        self.model = model
+        self.name = name
+        self.description = description
+        self.train_data = train_data
+        self.valid_data = valid_data
+        self.batch_size = batch_size
+        self.from_logits = from_logits
+        self.epochs = epochs
+        self.current_epoch = current_epoch
+        self.lr = lr
+        self.lr_scheduler = lr_scheduler
+        self.optimizer = optimizer
+        self.loss = loss
+        self.metrics = metrics
+
+        self.save_path = save_path + self.name + "/"
+        self.with_cp_save = with_cp_save
+        self.cp_freq = cp_freq
+        self.cp_dir = self.save_path + cp_dir
+
+        self.with_tensorboard = with_tensorboard
+        self.tb_dir = self.save_path + tb_dir
+        self.tb_hist_freq = tb_hist_freq
+
+        self.histories = []
+
+        self.__compile_model()
+        self.current_fit = 0
+        self.fit_model()
+
+
+    def __compile_model(self):
+        optimizer = self.optimizer if self.optimizer is not None \
+            else (tf.keras.optimizers.Adam(learning_rate=self.lr_scheduler) if self.lr_scheduler is not None
+                  else tf.keras.optimizers.Adam(lr=self.lr))
+        loss = self.loss if self.loss is not None \
+            else tf.keras.losses.BinaryCrossentropy(from_logits=self.from_logits)
+
+        self.model.compile(optimizer=optimizer,
+                           loss=loss,
+                           metrics=self.metrics)
+
+
+    def fit_model(self):
+        callbacks = []
+        if self.with_tensorboard:
+            callbacks.append(self.__get_tb_callback())
+        if self.with_cp_save:
+            callbacks.append(self.__get_cp_callback())
+        history = self.model.fit(
+            self.train_data,
+            validation_data=self.valid_data,
+            epochs=self.epochs,
+            callbacks=callbacks
+        )
+        self.histories.append(history)
+        self.__save_model(self.current_fit)
+        self.current_fit += 1
+        self.current_epoch += self.epochs
+
+
+    def __save_model(self, fit_n):
+        self.model.save(self.save_path + "SavedModel/" + fit_n)
+
+
+    def __get_cp_callback(self):
+        checkpoint_path = self.cp_dir + "cp-{epoch:04d}.ckpt"
+        checkpoint_dir = os.path.dirname(checkpoint_path)
+        self.model.save_weights(checkpoint_path.format(epoch=self.current_epoch))
+
+        return tf.keras.callbacks.ModelCheckpoint(
+            filepath=checkpoint_dir,
+            verbose=1,
+            save_weights_only=True,
+            save_freq=self.cp_freq*self.batch_size)
+
+
+    def __get_tb_callback(self):
+        logdir = os.path.join(self.tb_dir, datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+        return tf.keras.callbacks.TensorBoard(logdir, histogram_freq=self.tb_hist_freq)
+
+
+    def get_tensorlog_path(self):
+        return self.tb_dir
--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from data_access import ImageDaoKeras
 
+
 def main():
     print("Hey you! Smile!")
     dao_single_path = ImageDaoKeras(data_path="images/train")
     # dao_separate_paths = ImageDaoKeras(train_path="images/train", validation_path="images/test")
+
 
 if __name__ == "__main__":
     main()
