19ae5429e145a4c1891a3ba65341fb069a883f67
Rima Shahbazyan <rimashahbazyan@Rimas-MacBook-Pro.local>
2021-03-30 01:08:29 +0400

    Adding Inception V3

---
+++ InceptionV3_train.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 30 01:04:19 2021
+
+@author: rimashahbazyan
+"""
+
+import sys
+from data_access import ImageDaoKeras
+from model_training import KerasTrain, get_exp_scheduler
+from models.InceptionV3_pretrained import InceptionV3_pretrained
+
+
+def train_InceptionV3(data_path):
+    assert data_path is not None
+
+    dao_single_path = ImageDaoKeras(data_path=data_path)
+
+    model = InceptionV3_pretrained(dao_single_path).model
+    lr_scheduler = get_exp_scheduler(decay_step=1000)
+
+    trainer = KerasTrain(model=model,
+                         name="InceptionV3_pretrained_1000ep_genki_modified",
+                         train_data=dao_single_path.train_dataset,
+                         valid_data=dao_single_path.valid_dataset,
+                         iters=23,
+                         epochs=1000,
+                         lr_scheduler=lr_scheduler)
+
+    trainer.fit_model()
+
+
+def main(argv):
+    assert len(argv) > 0
+    data_path = argv[0]
+    print("Training VGG on data from", data_path)
+    train_InceptionV3(data_path)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ models/InceptionV3_pretrained.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 30 00:43:34 2021
+
+@author: rimashahbazyan
+"""
+
+from keras.applications import inception_v3
+from keras.models       import Model
+from keras.layers import Sequential, Dense, experimental, Dropout
+from keras import Input
+
+class InceptionV3_Pretrained:
+    def __init__(self,
+                 dataset=None):
+        """
+        Inception V3  model with pretrained weight from imagenet.
+
+        :param dataset: ImageDaoKeras or ImageDaoCustom object
+        """
+        assert dataset is not None
+        
+        data_augmentation = Sequential(
+            [
+                experimental.preprocessing.RandomFlip("horizontal",
+                                                             input_shape=(dataset.IMG_HEIGHT, dataset.IMG_WIDTH, 3)),
+                experimental.preprocessing.RandomRotation(0.15),
+                experimental.preprocessing.RandomZoom(0.1),
+            ]
+        )
+        
+        preprocess_input = inception_v3.preprocess_input
+        
+        original_model    = inception_v3()
+        bottleneck_input  = original_model.get_layer(index=0).input
+        bottleneck_output = original_model.get_layer(index=41).output
+        bottleneck_model  = Model(inputs=bottleneck_input,           
+                                  outputs=bottleneck_output)
+        
+        for layer in bottleneck_model.layers:
+            layer.trainable = False
+        
+        prediction_layer = Dense(2, activation='softmax')
+        
+        inputs =  Input(shape=(dataset.IMG_HEIGHT, dataset.IMG_WIDTH, 3))
+        x = data_augmentation(inputs)
+        x = preprocess_input(x)
+        x = bottleneck_model(x, training=False)
+        
+        outputs = prediction_layer(x)
+        
+
+        self.model = Model(inputs, outputs)
