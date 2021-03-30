fa848e2ed984d3bf7b00f08d29bf3e422028d099
Lilit Ghandilyan <lilit_ghandilyan@edu.aua.am>
2021-03-30 19:16:03 +0400

    minor

--- preprocessing/unzip_celeba.py
+++ preprocessing/unzip_celeba.py
@@ -9,10 +9,7 @@ def extract_zip(zip_path):
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         path = Path(zip_path)
         name = os.path.basename(path).split('.')[0]
-        print(name)
-        print(str(path.parent) + "/unzipped_" + name)
-        # zip_ref.extractall(str(path.parent) + "/unzipped_" + name)
-        # img_folder = str(path.parent) + "/unzipped_" + name + "/" + name
+        zip_ref.extractall(str(path.parent) + "/unzipped_" + name)
 
 
 extract_zip(zip_path)
