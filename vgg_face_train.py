1fadb27da297f19c6560bcc3d5a327783abf9a9a
ninakira <ninkirakosyan@gmail.com>
2021-04-05 17:42:47 +0400

    vgg_face_train adjustments

--- vgg_face_train.py
+++ vgg_face_train.py
@@ -10,14 +10,14 @@ def train_vgg_face(data_path=None):
                                     validation_path="../../../../data/augmented_celeba/validation")
 
     vgg_model = VGGFace().model
-    lr_scheduler = get_exp_scheduler(decay_step=200)
+    lr_scheduler = get_polymonial_scheduler()
 
     trainer = KerasTrain(model=vgg_model,
                          name="VGGFace_400ep_celeba_aug",
                          train_data=dao_single_path.train_dataset,
                          valid_data=dao_single_path.valid_dataset,
                          iters=5974,
-                         epochs=200,
+                         epochs=400,
                          with_early_stop=True,
                          lr_scheduler=lr_scheduler)
 
@@ -28,7 +28,7 @@ def reload_vgg_face_and_train():
     dao_single_path = ImageDaoKeras(train_path="../../../../data/augmented_celeba/train",
                                     validation_path="../../../../data/augmented_celeba/validation")
 
-    loaded_model = tf.keras.models.load_model('../SavedModels/VGGFace_200ep_celeba_aug/SavedModel/0/saved_model.pb')
+    loaded_model = tf.keras.models.load_model('../SavedModels/VGGFace_200ep_celeba_aug/SavedModel/0/')
     lr_scheduler = get_polymonial_scheduler()
 
     trainer = KerasTrain(model=loaded_model,
