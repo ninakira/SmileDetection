8d41c281c10d325b90312482293167689d8847b6
Lilit Ghandilyan <lilit_ghandilyan@edu.aua.am>
2021-04-06 01:37:32 +0400

    Add MobilenetV3 with Finetuning

--- mobilenet_train.py
+++ mobilenet_train.py
@@ -28,6 +28,7 @@ AUTOTUNE = tf.data.AUTOTUNE
 train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
 validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
 
+# Defining the model
 mobilenet = MobileNetV3()
 model = mobilenet.define_model()
 
@@ -36,9 +37,33 @@ model.compile(optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate),
               loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-# log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-# tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+log_dir = "~/code/SavedModels/Mobilenet/logs" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
 history = model.fit(train_dataset,
                     epochs=10,
-                    validation_data=validation_dataset)
+                    validation_data=validation_dataset,
+                    callbacks=[tensorboard_callback])
+
+model.base_model.trainable = True
+
+# Fine-tune from this layer onwards
+fine_tune_at = 50
+
+# Freeze all the layers before the `fine_tune_at` layer
+for layer in model.base_model.layers[:fine_tune_at]:
+    layer.trainable = False
+
+model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate/10),
+              metrics=['accuracy'])
+
+fine_tune_epochs = 10
+total_epochs = 50 + fine_tune_epochs
+
+history_fine = model.fit(train_dataset,
+                         epochs=total_epochs,
+                         initial_epoch=history.epoch[-1],
+                         validation_data=validation_dataset)
+
+model.save("~/code/SavedModels/Mobilenet")
