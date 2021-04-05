e8b6d3a7f6f257b9f6b257825a2073f01ec2400f
Lilit Ghandilyan <lilit_ghandilyan@edu.aua.am>
2021-04-06 00:35:52 +0400

    Add mobilenet train file

---
+++ mobilenet_train.py
@@ -0,0 +1,44 @@
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+from models.MobileNetV3 import MobileNetV3
+import tensorflow as tf
+import datetime
+import os
+
+PATH = 'data/augmented_celeba'
+
+train_dir = os.path.join(PATH, 'train')
+validation_dir = os.path.join(PATH, 'validation')
+test_dir = os.path.join(PATH, 'test')
+
+BATCH_SIZE = 128
+IMG_SIZE = (128, 128)
+
+train_dataset = image_dataset_from_directory(train_dir,
+                                             shuffle=True,
+                                             batch_size=BATCH_SIZE,
+                                             image_size=IMG_SIZE)
+
+validation_dataset = image_dataset_from_directory(validation_dir,
+                                                  shuffle=True,
+                                                  batch_size=BATCH_SIZE,
+                                                  image_size=IMG_SIZE)
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
+validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
+
+mobilenet = MobileNetV3()
+model = mobilenet.define_model()
+
+base_learning_rate = 0.0001
+model.compile(optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate),
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+# tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+history = model.fit(train_dataset,
+                    epochs=10,
+                    validation_data=validation_dataset)
---
+++ models/MobileNetV3.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+
+class MobileNetV3:
+    def __init__(self):
+        self.IMG_SHAPE = (128, 128, 3)
+        self.preprocess_input = tf.keras.applications.mobilenet_v3.preprocess_input
+        self.base_model = tf.keras.applications.MobileNetV3Large(input_shape=self.IMG_SHAPE,
+                                                                 include_top=False,
+                                                                 weights='imagenet')
+        self.base_model.trainable = False
+
+    def define_model(self):
+        inputs = tf.keras.Input(shape=self.IMG_SHAPE)
+        x = self.preprocess_input(inputs)
+        x = self.base_model(x, training=False)
+        x = tf.keras.layers.GlobalAveragePooling2D()(x)
+        x = tf.keras.layers.Dropout(0.2)(x)
+        outputs = tf.keras.layers.Dense(1)(x)
+        return tf.keras.Model(inputs, outputs)
