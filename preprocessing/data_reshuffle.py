34f1a63f0e7eda8d183584de29b356b6886ae61e
Lilit Ghandilyan <lilit_ghandilyan@edu.aua.am>
2021-04-05 15:53:43 +0400

    change data reshuffle to copy test augmented images

--- preprocessing/data_reshuffle.py
+++ preprocessing/data_reshuffle.py
@@ -1,20 +1,15 @@
 import os
 from shutil import copyfile
 
+copied_image_count = 0
+n_images_to_copy = 1000
 src = "/data/unzipped_celeba/celeba/train/1"
-dest = "/data/augmented_celeba/train/1"
+dest = "/data/test_images"
 
-for item in os.listdir("/data/unzipped_celeba/celeba/train/1"):
+for item in os.listdir(src):
+    copied_image_count += 1
     src_file = src + item
     dest_file = dest + item + 'original'
     copyfile(src_file, dest_file)
-
-# >>> print(len(os.listdir("/data/unzipped_celeba/celeba/train/1")))
-# 88069
-# >>> print(len(os.listdir("/data/augmented_celeba/train/1")))
-# 373603
-
-# >>> print(len(os.listdir("/data/unzipped_celeba/celeba/train/0")))
-# 94258
-# p>>> print(len(os.listdir("/data/augmented_celeba/train/0")))
-# 391004
+    if copied_image_count > n_images_to_copy:
+        break
