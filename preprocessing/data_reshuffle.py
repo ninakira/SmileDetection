6444e30d5eef95906dce8995a7f70ff71080683f
ghlilit <lilit.ghandilyanf@gmail.com>
2021-04-07 20:46:52 +0530

    Final augmentation celeba

--- preprocessing/data_reshuffle.py
+++ preprocessing/data_reshuffle.py
@@ -4,14 +4,14 @@ import re
 
 # copied_image_count = 0
 # n_images_to_copy = 1000
-# src = "/data/unzipped_celeba/celeba/train/1"
-# dest = "/data/test_images/"
-#
-# for item in os.listdir(src):
+src = "/data/original_celeba/celeba/validation/0/"
+dest = "/data/final_celeba/validation/0/"
+
+for item in os.listdir(src):
 #     copied_image_count += 1
-#     src_file = src + item
-#     dest_file = dest + item
-#     copyfile(src_file, dest_file)
+     src_file = src + item
+     dest_file = dest + "o" + item
+     copyfile(src_file, dest_file)
 #     if copied_image_count > n_images_to_copy:
 #         break
 #
--- preprocessing/data_tests.py
+++ preprocessing/data_tests.py
@@ -2,8 +2,8 @@ from keras.preprocessing.image import ImageDataGenerator
 
 datagen = ImageDataGenerator()
 
-train_it = datagen.flow_from_directory('/data/augmented_celeba/train/', class_mode='binary', batch_size=128)
-val_it = datagen.flow_from_directory('/data/augmented_celeba/validation/', class_mode='binary', batch_size=128)
+train_it = datagen.flow_from_directory('/data/final_celeba/train/', class_mode='binary', batch_size=128)
+val_it = datagen.flow_from_directory('/data/final_celeba/validation/', class_mode='binary', batch_size=128)
 
 print(train_it)
 print(val_it)
