5400c007669708697f3b668c0c14b35f14180f11
ninakira <ninkirakosyan@gmail.com>
2021-04-25 15:34:14 +0400

    Eff B3 adjustment

--- training/efficientnet_b3_train.py
+++ training/efficientnet_b3_train.py
@@ -52,9 +52,12 @@ class EfficientNetTrainer:
         self.trainer.compile_model(optimizer)
         self.trainer.fit_model(epochs, with_early_stop=True, early_stop_patience=3, save_weights_only=False)
 
-    def train_saved_model(self, name, fit, epochs, lr):
+    def train_saved_model(self, name, fit, epochs, lr, frozen=True):
         reconstructed_model = load_saved_model(name, fit)
         self.set_trainer(reconstructed_model, name)
+        if not frozen:
+            for layer in self.model.layers:
+                layer.trainable = True
 
         optimizer = tf.keras.optimizers.Adam(lr)
         self.trainer.compile_model(optimizer)
@@ -73,7 +76,7 @@ efficient_net3_trainer = EfficientNetTrainer(celeba_train, celeba_validation)
 #                                       fine_tune_epochs=20,
 #                                       fine_tune_lr=1e-5)
 
-efficient_net3_trainer.train_saved_model('EfficientNet_b3_celeba', 1, 20, 1e-5)
+efficient_net3_trainer.train_saved_model('EfficientNet_b3_celeba', 0, 20, 1e-5, False)
 
 
 # test_model(model, img_size=IMG_SIZE, model_name='EfficientNet_b3_celeba')
